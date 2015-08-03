0685903bc2cf1b60aa104d1045cb40238180f941
Luminarys <kizunanohikari@gmail.com>
2015-08-03 18:09:28 -0500

    Testing updates

--- gochat_test.go
+++ gochat_test.go
@@ -2,7 +2,6 @@ package gochat
 
 import (
 	"testing"
-	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -45,35 +44,4 @@ func TestMessage(t *testing.T) {
 	c.Say("Message test")
 	c.Part()
 	bot.Quit()
-}
-
-func TestModules(t *testing.T) {
-	bot, err := NewBot("irc.rizon.net:6666", "go-bot")
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	LoadDefaultModules(bot)
-
-	c := bot.JoinChan("#go-bot-test")
-	time.Sleep(10 * time.Second)
-	c.DumpLogs()
-	c.Part()
-	bot.Quit()
-}
-
-//Loads the default provided Modules
-func LoadDefaultModules(bot.Bot) {
-	bot.AddModule(&PingMod{})
-	um := &URLMod{}
-	um.Init()
-	bot.AddModule(um)
-	sm := &SedMod{}
-	sm.Init()
-	bot.AddModule(sm)
-	bot.AddModule(&QuoteMod{})
-	qm := &CuteMod{}
-	qm.Init()
-	bot.AddModule(qm)
-	bot.AddModule(&ChanJoinMod{})
 }
